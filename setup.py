23da3d5d0f4bc8103ff68c27db82506c728dde7e
ely-as <mail@ely.as>
2022-01-08 23:13:03 -0800

    Release 0.2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(Path(__file__).parent / 'README.md') as f:
 setup(
     name='django-envconfig',
     packages=find_packages(exclude=['tests']),
-    version='0.2.3',
+    version='0.2.4',
     license='MIT',
     description='Configure Django using environment variables.',
     long_description=long_description,
